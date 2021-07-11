b506139272fa6dd0a48c63ebcd5b2d7a24cae057
Yeon Jae Park <yeonjaepark@Yeonui-MacBookAir.local>
2021-07-11 13:01:46 +1000

    Modified Login and Register page

--- resources/js/components/Routes/Login.js
+++ resources/js/components/Routes/Login.js
@@ -1,17 +1,13 @@
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import {withRouter, NavLink} from "react-router-dom";
 
-
-const StyledForm = styled(Form)`
-    max-width: ${window.innerWidth*0.4}px;
-    margin-top: ${window.innerHeight*0.3}px;
-    margin-left: ${window.innerWidth*0.3}px;
-
-`;
-
+const Title = styled.h1`
+  text-align: center;
+    margin-bottom: 10%;
+`
 const StyledButton = styled(Button)`
     width: 100%;
 `;
@@ -31,57 +27,63 @@ const LoginPage = () => {
     };
 
     return (
-        <StyledForm
-            name="normal_login"
-            className="login-form"
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Username!',
-                    },
-                ]}
-            >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Password!',
-                    },
-                ]}
-            >
-                <Input
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                />
-            </Form.Item>
-            <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
+        <Row type="flex" justify="center" align="middle" style={{minHeight: '100vh'}}>
+            <Col span={10}>
+                <Title>Welcome to Soccer App!</Title>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
 
-                <ForgotPassword className="login-form-forgot" href="">
-                    Forgot password
-                </ForgotPassword>
-            </Form.Item>
+                        <ForgotPassword className="login-form-forgot" href="">
+                            Forgot password
+                        </ForgotPassword>
+                    </Form.Item>
 
-            <Form.Item>
-                <StyledButton type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                </StyledButton>
-                Or <SLink to="/register">register now!</SLink>
-            </Form.Item>
-        </StyledForm>
+                    <Form.Item>
+                        <StyledButton type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </StyledButton>
+                        Or <SLink to="/register">register now!</SLink>
+                    </Form.Item>
+                </Form>
+            </Col>
+        </Row>
+
 
     );
 
--- resources/js/components/Routes/Register.js
+++ resources/js/components/Routes/Register.js
@@ -4,15 +4,44 @@ import styled from "styled-components";
 
 const Title = styled.h1`
     text-align: center;
-    margin-top: ${window.innerHeight*0.1}px;
+    margin-bottom: 10%;
 `
 
-const StyledForm = styled(Form)`
-    max-width: ${window.innerWidth*0.4}px;
-    margin-top: ${window.innerHeight*0.1}px;
-    margin-left: ${window.innerWidth*0.3}px;
+const StyledButton = styled(Button)`
+  float: right;
+`
 
-`;
+const tailFormItemLayout = {
+    wrapperCol: {
+        xs: {
+            span: 24,
+            offset: 0,
+        },
+        sm: {
+            span: 16,
+            offset: 8,
+        },
+    },
+};
+
+const formItemLayout = {
+    labelCol: {
+        xs: {
+            span: 24,
+        },
+        sm: {
+            span: 8,
+        },
+    },
+    wrapperCol: {
+        xs: {
+            span: 24,
+        },
+        sm: {
+            span: 16,
+        },
+    },
+};
 
 const Register = () => {
     const [form] = Form.useForm();
@@ -22,46 +51,119 @@ const Register = () => {
     };
 
     return(
-        <>
-        <Title>Register</Title>
-        <StyledForm
-            form={form}
-            name="register"
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="email"
-                label="E-mail"
-                rules={[
-                    {
-                        type: 'email',
-                        message: 'The input is not valid E-mail!',
-                    },
-                    {
-                        required: true,
-                        message: 'Please input your E-mail!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
+        <Row type="flex" justify="center" align="middle" style={{minHeight: '100vh'}}>
+            <Col span={8}>
+                <Title>Register</Title>
+                <Form
+                    {...formItemLayout}
+                    form={form}
+                    name="register"
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="email"
+                        label="E-mail"
+                        rules={[
+                            {
+                                type: 'email',
+                                message: 'The input is not valid E-mail!',
+                            },
+                            {
+                                required: true,
+                                message: 'Please input your E-mail!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-            <Form.Item
-                name="password"
-                label="Password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-                hasFeedback
-            >
-                <Input />
-            </Form.Item>
+                    <Form.Item
+                        name="password"
+                        label="Password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                        hasFeedback
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-        </StyledForm>
-        </>
+                    <Form.Item
+                        name="confirm"
+                        label="Confirm Password"
+                        dependencies={['password']}
+                        hasFeedback
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please confirm your password!',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
+
+                                    return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="confirm"
+                        label="Confirm Password"
+                        dependencies={['password']}
+                        hasFeedback
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please confirm your password!',
+                            },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
+
+                                    return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                                },
+                            }),
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="nickname"
+                        label="Nickname"
+                        tooltip="What do you want others to call you?"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your nickname!',
+                                whitespace: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item {...tailFormItemLayout}>
+                        <StyledButton type="primary" htmlType="submit">
+                            Register
+                        </StyledButton>
+                    </Form.Item>
+
+                </Form>
+            </Col>
+        </Row>
+
 
     )
 }
