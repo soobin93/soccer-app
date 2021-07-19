8b5e9cec3c3fcee731d6f017f41b761c8c815ec9
Yeon Jae Park <yeonjaepark@Yeonui-MacBookAir.local>
2021-07-19 22:06:21 +1000

    #15 Fixed Indentation

--- resources/js/routes/login.js
+++ resources/js/routes/login.js
@@ -17,34 +17,34 @@ const StyledButton = styled(Button)`
 
 const LoginPage = () => {
 
-  const [form] = Form.useForm();
-  const [user, setUser] = useUser();
-  const history = useHistory();
+    const [form] = Form.useForm();
+    const [user, setUser] = useUser();
+    const history = useHistory();
 
     const onFinish = (data) => {
         event.preventDefault();
 
 
-    UserApi.login(data).then(function (response) {
-      setUser(response.data.user);
-      history.push('/admin/user');
-    }).catch(function (error) {
-      if (error.response.data.hasOwnProperty('errors')) {
-        const errors = error.response.data.errors;
-        let fields = [];
-        for (const inputKey in errors) {
-          if (errors.hasOwnProperty(inputKey)) {
-            fields.push({
-              name: inputKey,
-              errors: [errors[inputKey]]
-            });
-          }
-        }
+        UserApi.login(data).then(function (response) {
+            setUser(response.data.user);
+            history.push('/admin/user');
+        }).catch(function (error) {
+            if (error.response.data.hasOwnProperty('errors')) {
+                const errors = error.response.data.errors;
+                let fields = [];
+                for (const inputKey in errors) {
+                    if (errors.hasOwnProperty(inputKey)) {
+                        fields.push({
+                            name: inputKey,
+                            errors: [errors[inputKey]]
+                        });
+                    }
+                }
 
-        form.setFields(fields);
-      }
-    });
-  };
+                form.setFields(fields);
+            }
+        });
+    };
 
     const logOut = () => {
         UserApi.logout().then(function (response) {
@@ -52,57 +52,57 @@ const LoginPage = () => {
         });
     }
 
-  return (
-    <Row type="flex" justify="center" align="middle" style={{minHeight: '80vh'}}>
-      <Col span={18}>
-        <Title>Welcome to Soccer App!</Title>
-        <Form
-          form={form}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: 'Please type your email',
-              }
-            ]}
-          >
-            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Email"/>
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please type your password',
-              },
-            ]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon"/>}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item>
-            <StyledButton type="primary" htmlType="submit" className="login-form-button">
-              Log in
-            </StyledButton>
-          </Form.Item>
-          <Form.Item>
-            <StyledButton type="danger" onClick={logOut}>
-              Log Out
-            </StyledButton>
-          </Form.Item>
-        </Form>
-      </Col>
-    </Row>
-  );
+    return (
+        <Row type="flex" justify="center" align="middle" style={{minHeight: '80vh'}}>
+            <Col span={18}>
+                <Title>Welcome to Soccer App!</Title>
+                <Form
+                    form={form}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please type your email',
+                            }
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Email"/>
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please type your password',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon"/>}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <StyledButton type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </StyledButton>
+                    </Form.Item>
+                    <Form.Item>
+                        <StyledButton type="danger" onClick={logOut}>
+                            Log Out
+                        </StyledButton>
+                    </Form.Item>
+                </Form>
+            </Col>
+        </Row>
+    );
 }
 
 export default LoginPage;
