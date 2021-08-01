bafe39117bdcf64a312a460f73e8c600b06278cb
Yeon Jae Park <yeonjaepark@Yeonui-MacBookAir.local>
2021-08-01 13:33:57 +1000

    #27 Created User Avatar

--- resources/js/components/LandingPage.js
+++ resources/js/components/LandingPage.js
@@ -1,17 +1,65 @@
 import React from "react";
 import {useUser} from "components/contexts/UserContext";
+import {Avatar, Card, Row} from "antd";
+import Meta from "antd/es/card/Meta";
+import {AntDesignOutlined, UserOutlined} from "@ant-design/icons";
+import styled from "styled-components";
+import {NavLink} from "react-router-dom";
 
+const CardContainer = styled.div`
+  justify-content: center;
+  display: flex;
+`;
+
+const StyledAvatar = styled(Avatar)`
+  margin-right: 20px;
+`
+
+const StyledUserOutlined = styled(UserOutlined)`
+  margin-right: 10px;
+  font-size: large;
+`
+
+const Title = styled.div`
+  font-size: larger;
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+const StyledLink = styled(NavLink)`
+  font-size: medium;
+`
 
 const LandingPage = () => {
 
   const [user] = useUser();
 
   return (
-    <div>
-      <h1>This is landing page!</h1>
-      <h2>Welcome {user.name}!</h2>
-      {user.avatar && <img src={`/storage/avatars/${user.avatar}`} alt="avatar" />}
-    </div>
+    <CardContainer>
+      <Card
+        style={{width: '90%', borderWidth:'2px', borderRadius:'10px'}}
+        bodyStyle={{padding: '15px'}}
+      >
+        <Meta
+          avatar={
+            <StyledAvatar
+              icon={<AntDesignOutlined />}
+              size={{ xs: 120, sm: 130, md: 140, lg: 150, xl: 160, xxl: 170 }}
+              src={`/storage/avatars/${user.avatar}`}
+            />}
+          title={
+            <Title>{user.name}</Title>
+          }
+          description={
+            <>
+            <StyledUserOutlined/>
+            <StyledLink to='/profile'>Profile</StyledLink>
+            </>
+          }
+        />
+      </Card>
+    </CardContainer>
+
   );
 }
 
